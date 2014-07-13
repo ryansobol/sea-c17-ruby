70cf45766364641c5ccc799b8d4630b4a2772bf4
Colin McKenna <colinjmckenna@gmail.com>
2014-07-13 16:13:53 -0700

    Exercise 6 & 7

--- class2/6_bigger_better_favorite_number.rb
+++ class2/6_bigger_better_favorite_number.rb
@@ -3,3 +3,11 @@
 # Write a program that asks for a person’s favorite number. Have your program
 # add 1 to the number, and then suggest the result as a bigger and better
 # favorite number. (Do be tactful about it, though.)
+
+print "You may have noticed, I'm a computer.  I compute things. Here, let me prove it.  What is your favorite number?"
+x = gets.chomp
+y = x.to_i + 1
+best = y.to_s
+puts  "As a computer I need to tell you that " + best + " is a larger than " + x + ". And I think we all know bigger is better." 
+puts ''
+puts "So...there's that."
--- class2/7_angry_boss.rb
+++ class2/7_angry_boss.rb
@@ -5,3 +5,13 @@
 # example, if you type in I want a raise, it should yell back like this:
 #
 # WHADDAYA MEAN "I WANT A RAISE"?!? YOU'RE FIRED!!
+
+puts ''
+puts "Can't you see I'm extremely busy right now?"
+puts "What, in the name of God is so important that you needed to inturrupt me?"
+print "Spit it out!  "
+humble_request = gets.chomp
+puts ''
+puts 'YOU CAN\'T BE SERIOUS? "' + humble_request.upcase + '"!?!?!?'
+puts 'YOU ARE FIRED!  GET OUT! DON\'T SHOW YOUR FACE AROUND HERE EVER AGAIN!!!'
+puts ''
