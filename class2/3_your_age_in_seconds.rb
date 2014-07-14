3f7b7dec13c3010775138d846086732cf162bc72
Ryan Sobol <contact@ryansobol.com>
2014-07-13 23:16:11 -0700

    Cosmetic suggestions

--- class2/3_your_age_in_seconds.rb
+++ class2/3_your_age_in_seconds.rb
@@ -4,6 +4,6 @@
 # How many seconds old are you? (I’m not going to check your answer, so be as
 # accurate — or not — as you want.)
 
-puts 60 * 60 * 24 * 365 * 23 + (60 * 60 * 24 * 14)
+puts 60 * 60 * 24 * 365 * 23 + 60 * 60 * 24 * 14
 
 # this was assuming that I'm 23 years and 14 days old.
