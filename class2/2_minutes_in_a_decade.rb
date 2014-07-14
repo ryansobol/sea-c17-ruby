eefc6cdb33ff7246fbc26352d926b1feea579f9a
Ryan Sobol <contact@ryansobol.com>
2014-07-14 16:33:23 -0700

    Cosmetic suggestions

--- class2/2_minutes_in_a_decade.rb
+++ class2/2_minutes_in_a_decade.rb
@@ -2,4 +2,4 @@
 # 4 points
 # How many minutes are in a decade?
 
-puts 60*24*365*10
+puts 60 * 24 * 365 * 10
--- class2/3_your_age_in_seconds.rb
+++ class2/3_your_age_in_seconds.rb
@@ -3,4 +3,4 @@
 # How many seconds old are you? (I’m not going to check your answer, so be as
 # accurate — or not — as you want.)
 
-puts 26*365*24*60*60
+puts 26 * 365 * 24 * 60 * 60
