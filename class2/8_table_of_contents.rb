225c7e6ddf4d433da31d3839f2ad9392bd82437d
John Fahey <faheydesign@gmail.com>
2014-07-15 20:12:36 -0700

    updating file

--- class2/8_table_of_contents.rb
+++ class2/8_table_of_contents.rb
@@ -1,19 +1,14 @@
-# Section 6.2 on page 32
-# 4 points
-# Here’s something for you to do in order to play around more with center,
-# ljust, and rjust: write a program that will display a table of contents so
-# that it looks like this:
-#
+
 #                 Table of Contents
 #
 # Chapter 1:  Getting Started                page  1
 # Chapter 2:  Numbers                        page  9
 # Chapter 3:  Letters                        page 13
 
-puts ' '
-line_width = 51
+puts
+line_width = 50
 puts 'Table of Contents'.center(line_width)
-puts ' '
+puts
 puts 'Chapter 1:'.ljust(line_width/4) + 'Getting Started'.ljust(line_width/1.5) + 'page '.ljust(line_width/25) +  "1".rjust(line_width/25)
 puts 'Chapter 2:'.ljust(line_width/4) + 'Numbers'.ljust(line_width/1.5) + 'page '.ljust(line_width/25) + '9'.rjust(line_width/25)
 puts 'Chapter 3:'.ljust(line_width/4) + 'Letters'.ljust(line_width/1.5) + 'page '.ljust(line_width/25) + '13'.rjust(line_width/25)
