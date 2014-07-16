ddbb10392fbc845e5627901c522f6412ff900ceb
Cody Brewer <cody.brewer.1@gmail.com>
2014-07-16 09:36:26 -0700

    Fix pluralization in 'bottles' problem

--- class3/1_99_bottles_of_beer_on_the_wall.rb
+++ class3/1_99_bottles_of_beer_on_the_wall.rb
@@ -1,63 +1,37 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Section 7.5 on page 49
 # 4 points
 #
 # Write a program that prints out the lyrics to that beloved classic,
-# "99 Bottles of Beer on the Wall."
+# "99 Bottles of Beer on the Wall." Here's what the output should look like:
+#
+# 99 bottles of beer on the wall, 99 bottles of beer!
+# Take one down, pass it around, 98 bottles of beer on the wall!
+# 98 bottles of beer on the wall, 98 bottles of beer!
+# Take one down, pass it around, 97 bottles of beer on the wall!
+# ...
+# 2 bottles of beer on the wall, 2 bottles of beer!
+# Take one down, pass it around, 1 bottle of beer on the wall!
+# 1 bottle of beer on the wall, 1 bottle of beer!
+# Take one down, pass it around, no more bottles of beer on the wall!
 
 bottles = 99
 bottle_plural = "bottles"
-while bottles >= 1
+while bottles
 
-  if bottles == 1
-    puts "#{bottles} more bottle of beer on the wall, only "\
-         "#{bottles} bottle of beer!"
-    puts "Take it down, pass it around, no more bottles of beer on the wall!"
-  elsif bottles > 1
-    if bottles == 2
+    puts "#{bottles} #{bottle_plural} of beer on the wall,"\
+         " #{bottles} #{bottle_plural} of beer!"
+    print "Take one down, pass it around, "
+    bottles -= 1
+
+    if bottles == 1
       bottle_plural = "bottle"
     end
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer!"
-  puts "Take one down, pass it around, #{bottles-1} #{bottle_plural} "\
-       "of beer on the wall!"
-  end
-  bottles -= 1
+
+    if bottles == 0
+      break
+    end
+
+    puts "#{bottles} #{bottle_plural} of beer on the wall!"
 end
+
+puts "no more bottles of beer on the wall!"
