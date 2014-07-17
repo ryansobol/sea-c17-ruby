4804d7ff0d2e963a279538370a39dbd8bffd91b1
Pete Mitchell <petemmitchell@gmail.com>
2014-07-16 17:19:29 -0700

    Adding homework part 1

--- class3/1_99_bottles_of_beer_on_the_wall.rb
+++ class3/1_99_bottles_of_beer_on_the_wall.rb
@@ -3,3 +3,21 @@
 #
 # Write a program that prints out the lyrics to that beloved classic,
 # "99 Bottles of Beer on the Wall."
+
+beers = 100
+verse = 0
+
+while verse < 99
+  verse += 1
+
+  puts "#{beers - verse} bottles of beer on the wall,"
+  puts "#{beers - verse} bottles of beer,"
+  puts "You take one down and pass it around,"
+  puts "#{beers - verse} bottles of beer on the wall!"
+  puts
+end
+
+puts "No more bottles of beer on the wall,"
+puts "No more bottles of beer,"
+puts "Go to the store and buy some more,"
+puts "99 bottles of beer on the wall..."
