05315ff0ffadcd5eb0480699725cf722535ffa6c
Michael Lee <mlee1974@gmail.com>
2014-07-17 18:45:25 -0700

    updated

--- class3/3_deaf_grandma_extended.rb
+++ class3/3_deaf_grandma_extended.rb
@@ -6,24 +6,8 @@
 # shout BYE three times in a row. Make sure to test your program: if you shout
 # BYE three times but not in a row, you should still be talking to Grandma.
 
-#puts "SAY SOMETHING, SONNY!"
 bye_count = 0
-
-#while bye_count.to_i < 3
-#  input = gets.chomp
-#  if input == "BYE" && bye_count < 3
-#    bye_count = bye_count.to_i + 1
-#    puts "SAY AGAIN"
-#  end
-#  if input == "BYE" && bye_count == 3
-#    puts "NO, NOT SINCE 1938!"
-#  elsif input != "BYE"
-#    bye_count = 0
-#    puts "HUH!  SPEAK UP, SONNY!"
-#  end
-#end
-
-while true #bye_count.to_i < 3
+while true
   puts "SAY SOMETHING, SONNY!"
   input = gets.chomp
   if input == "BYE" && bye_count < 3
