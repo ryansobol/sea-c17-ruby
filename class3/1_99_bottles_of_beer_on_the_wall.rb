beb238fcf74f2b8a91f282e5e127ed75623e840b
John Fahey <faheydesign@gmail.com>
2014-07-19 09:53:54 -0700

    Adding solution for bottles of beer

--- class3/1_99_bottles_of_beer_on_the_wall.rb
+++ class3/1_99_bottles_of_beer_on_the_wall.rb
@@ -15,3 +15,15 @@
 # Take one down, pass it around, no more bottles of beer on the wall!
 # No more bottles of beer on the wall, no more bottles of beer!
 # Go to the store and buy some more, 99 bottles of beer on the wall!
+i = 99
+while i > 0
+  puts i.to_s + ' bottles of beer on the wall, ' + i.to_s + ' bottles of beer!'
+  i -= 1
+  if i > 0
+    puts 'Take one down, pass it around, ' + i.to_s + ' bottles of beer on the wall!'
+  else
+    puts 'Take one down, pass it around, no more bottles of beer on the wall!'
+    puts 'No more bottles of beer on the wall, no more bottles of beer!'
+    puts 'Go to the store and buy some more, 99 bottles of beer on the wall!'
+  end
+end
