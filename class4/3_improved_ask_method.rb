3f8dfeb54bf0b604ce56ebb4854714c2d2d43458
Pete Mitchell <petemmitchell@gmail.com>
2014-07-22 17:31:55 -0700

    Adding 2 additional HW solutions

--- class4/3_improved_ask_method.rb
+++ class4/3_improved_ask_method.rb
@@ -11,7 +11,31 @@
 # Hint: I've already structured your program. Aren't you lucky? :)
 
 def ask(question)
-  # your solution here
+  while true
+    puts question
+    reply = gets.chomp.downcase
+      if reply == "yes"
+        return true
+      elsif reply == "no"
+        return false
+      else
+        puts "Please answer \"yes\" or \"no\"."
+    end
+  end
 end
 
-puts ask('Do you like eating tacos?')
+puts "Hello, and thank you for..."
+puts
+ask "Do you like eating tacos?"
+ask "Do you like eating burritos?"
+wets_bed = ask "Do you wet the bed?"
+ask "Do you like eating chimichangas?"
+ask "Do you like eating sopapillas?"
+puts "Just a few more questions..."
+ask "Do you like drinking horchata?"
+ask "Do you like eating flautas?"
+puts
+puts "DEBRIEFING:"
+puts "Thank you for..."
+puts
+puts wets_bed
--- class4/4_old_school_roman_numerals.rb
+++ class4/4_old_school_roman_numerals.rb
@@ -36,7 +36,32 @@
 #   VIIII
 
 def old_school_roman_numeral(num)
-  # your solution here
+  values = [1000, 500, 100, 50, 10, 5, 1]
+
+  m_remainder = num % values[0]
+  d_remainder = num % values[1]
+  c_remainder = num % values[2]
+  l_remainder = num % values[3]
+  x_remainder = num % values[4]
+  v_remainder = num % values[5]
+
+  m_number = num / values[0]
+  d_number = m_remainder / values[1]
+  c_number = d_remainder / values[2]
+  l_number = c_remainder / values[3]
+  x_number = l_remainder / values[4]
+  v_number = x_remainder / values[5]
+  i_number = v_remainder / values[6]
+
+  m = "M" * m_number
+  d = "D" * d_number
+  c = "C" * c_number
+  l = "L" * l_number
+  x = "X" * x_number
+  v = "V" * v_number
+  i = "I" * i_number
+
+  puts m + d + c + l + x + v + i
 end
 
 input = ARGV[0].to_i
@@ -47,3 +72,4 @@ if input <= 0 || input > 3000
 end
 
 puts old_school_roman_numeral(input)
+
