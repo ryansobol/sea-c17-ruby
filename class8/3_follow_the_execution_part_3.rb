567aa1ae82afc5c78798a9a0c8f9b510e346e19b
Pete Mitchell <petemmitchell@gmail.com>
2014-08-04 20:36:45 -0700

    Adding 3 HW solutions

--- class8/3_follow_the_execution_part_3.rb
+++ class8/3_follow_the_execution_part_3.rb
@@ -16,9 +16,16 @@
 # Hint: Don't over think this one. The simpler the better.
 
 def follow_the_execution(method_param, &block)
-  # your code here
+  puts "Executing the method"
+  puts "method_param is #{method_param}"
+  block.call
 end
 
+meal = "lunch"
+
 follow_the_execution("dinner") do |block_param|
-  # your code here
+  block_param = meal
+  puts "Executing the block"
+  puts "block_param is #{block_param}"
+  end
 end
